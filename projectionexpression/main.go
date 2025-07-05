32b89f3e202066b243b7a78e8181b79b6efab61c
David Robertson <david.m.robertson1@gmail.com>
2025-07-05 21:52:38 +0100

    Try GHA for running tests

---
+++ projectionexpression/main.go
@@ -0,0 +1,5 @@
+// Package projectionexpression parses DynamoDB update expressions, as
+// documented at https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.UpdateExpressions.html
+//
+// TODO: see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Constraints.html#limits-expression-parameters for max limits
+package projectionexpression
